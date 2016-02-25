30856567fb800167ecb0bef7cb471b993816f421
aan9210j <ashwinianene@gmail.com>
2016-02-24 18:17:05 -0800

    added code to terminate the program

--- src/veri.asm
+++ src/veri.asm
@@ -1,40 +1,45 @@
 .MODEL SMALL
     .STACK 200H
     .DATA
-NUM1 DB 12
-NUM2 DB 3
-VAL  DB ?
-MSG1 DB "The sum is : $"
+
 
     .CODE
+
     call next
 	next: pop si
 	jmp xyz
 	nop
 	nop
-xyz: call verify	
-	nop
+xyz: 
+
+;call verify	
+;	nop
 	
-verify:
+;verify:
 	
 	
 	mov bh, 0
-	mov ah, byte ptr [cs:si]
-loop1:	cmp byte ptr [cs:si],97H
+;	mov ah, byte ptr [cs:si]
+loop1:	cmp byte ptr [cs:si],97H ;end of file canary
 	je end_label
 	cmp byte ptr [cs:si], 0ebh
 	jne loop_label
-	cmp byte ptr [cs:si+1],3
+	cmp byte ptr [cs:si+1],2
 	jne loop_label
-	cmp byte ptr [cs:si+2], 27H
+	cmp byte ptr [cs:si+2], 27H ;key share canary
 	jne loop_label
-	add si,2
-	add si,1
-	xor bh,byte [cs:si]
+	;add si,2
+	;add si,1
+	xor bh,byte ptr [cs:si+3]
 loop_label: add si,1
     jmp loop1
 end_label: 
 
-		
+	jmp end_program	
     nop
-	END 
+end_program:	nop
+	nop
+	mov ah,4ch
+    int 21H
+
+	end
